c0d197fc024ce86ee6e61e24cfcc058ecd691eff
YunxingSu <yunxing_su@brown.edu>
2016-11-28 19:23:00 -0500

    Add files via upload
    Qt codes

---
+++ qt/main.cpp
@@ -0,0 +1,14 @@
+#include "heat_equation.h"
+#include <QApplication>
+#include <QTextStream>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    heat_equation w;
+    w.show();
+
+
+
+    return a.exec();
+}
