c28dc2f03c34bc8c4f6582eb5dbea71ef5f08d1b
username <helena_liu@brown.edu>
2016-11-30 10:53:10 -0500

    setup guide

---
+++ CMakeFiles/FEM.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/hliu4/class/FEM-A.git/FEM-A/inc/algorithm.cpp" "/users/hliu4/class/FEM-A.git/FEM-A/CMakeFiles/FEM.dir/inc/algorithm.cpp.o"
+  "/users/hliu4/class/FEM-A.git/FEM-A/inc/print_result.cpp" "/users/hliu4/class/FEM-A.git/FEM-A/CMakeFiles/FEM.dir/inc/print_result.cpp.o"
+  "/users/hliu4/class/FEM-A.git/FEM-A/inc/read_input.cpp" "/users/hliu4/class/FEM-A.git/FEM-A/CMakeFiles/FEM.dir/inc/read_input.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/gpfs/runtime/opt/eigen/3.2.1/include/eigen3"
+  "inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/FEM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FEM.dir/inc/algorithm.cpp.o"
+  "CMakeFiles/FEM.dir/inc/print_result.cpp.o"
+  "CMakeFiles/FEM.dir/inc/read_input.cpp.o"
+  "libFEM.pdb"
+  "libFEM.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FEM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
