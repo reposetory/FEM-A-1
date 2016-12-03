50360fe507f5063f9ac9c8b7c2cb8e27fccdcbdd
YunxingSu <yunxing_su@brown.edu>
2016-12-02 21:22:40 -0500

    Add files via upload
    new version of read_input codes

--- FEM-A-week2/inc/read_input.cpp
+++ FEM-A-week2/inc/read_input.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 const double PI = 3.141592653589793238463;
 
-void read_input(string filename, vector<double>& grids, double& h_space, double& k_time, double& T_end) {
+void read_input(string filename, string solvername, vector<double>& grids, double& h_space, double& k_time, double& T_end) {
 	/*
 	for the demo code, instead of read the requied information form file,
 	we used a standard test input for 1D sample
@@ -58,6 +58,7 @@ void read_input(string filename, vector<double>& grids, double& h_space, double&
 	h_space = h; 
 	k_time = k; 
 	T_end = T;
+	solvername = num_scheme;
 	N_space = 2 * PI / h_space - 1; 
 
 	for (i; i <= N_space; i++) {
